947c55c9c4c6db4563af3b67aa6a5fc1e1711ee7
Walidi <waab18ab@student.cbs.dk>
2022-03-23 12:32:52 +0100

    adding imports again

--- src/components/Profile/MyProfile.js
+++ src/components/Profile/MyProfile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import './MyProfile.css'
 import Axios from 'axios';
 import  {withRouter } from 'react-router-dom';
