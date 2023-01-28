6c98415d823aea4d73422364eb7c1b640727fd1b
Walidi <waab18ab@student.cbs.dk>
2023-01-28 20:28:24 +0100

    :....:

--- src/components/Profile/UserProfile.js
+++ src/components/Profile/UserProfile.js
@@ -352,7 +352,7 @@ function UserProfile () {
              //Build a URL from the file
             const fileURL = URL.createObjectURL(file);
              //Open the URL on new Window
-            window.open(fileURL, "test.pdf");
+            window.open(fileURL);
           });
    }};
 
